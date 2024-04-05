5a1eb62ed383ee6f8bd4d0c16adb1c0625b5f047
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2024-04-05 16:09:00 -0300

    feat: método para upload das fotos

--- services/galeria.ts
+++ services/galeria.ts
@@ -1,7 +1,16 @@
-import { list, ref, ListResult, StorageReference } from 'firebase/storage';
+import { list, ref, ListResult, StorageReference, getDownloadURL, uploadBytes } from 'firebase/storage';
 import { storage } from './firebase';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 
 export const getNewestPhotos = async (): Promise<ListResult> => {
     const galleryRef: StorageReference = ref(storage, 'galeria');
     return await list(galleryRef, { maxResults: 3 });
+};
+
+export const uploadPhoto = async (image: File | null) => {
+    if (image === null) return;
+    const imageRef = ref(storage, `galeria/${image?.name}`);
+    uploadBytes(imageRef, image).then(() => {
+        window.location.reload();
+    });
 };
