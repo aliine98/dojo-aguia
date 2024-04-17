e7220b3e449b3d7476f5338da382976e8d1e033b
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2024-04-17 18:29:41 -0300

    feat: adiciona método para atualizar info de aluno
    Além do novo método, também muda outros métodos para usar await e adiciona id do doc na lista de alunos

--- services/studentsDb.ts
+++ services/studentsDb.ts
@@ -1,23 +1,16 @@
 import { IStudent } from '@/model/Student';
-import { addDoc, collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, getDocs, updateDoc } from 'firebase/firestore';
 import { ref, uploadBytes, getDownloadURL, deleteObject } from 'firebase/storage';
 import { db, storage } from './firebase';
-import { useRouter } from 'next/navigation';
 
 export const addStudent = async (student: IStudent) => {
     const photoRef = ref(storage, `alunos/${student.photoName}`);
-    uploadBytes(photoRef, student.photoFile).then(snapshot => {
-        getDownloadURL(snapshot.ref).then((url: string) => {
-            addDoc(collection(db, 'alunos'), {
-                name: student.name,
-                belt: student.belt,
-                photoUrl: url,
-                id: crypto.randomUUID(),
-            }).then(() => {
-                useRouter().refresh();
-                // window.location.reload();
-            });
-        });
+    const snapshot = await uploadBytes(photoRef, student.photoFile);
+    const url = await getDownloadURL(snapshot.ref);
+    await addDoc(collection(db, 'alunos'), {
+        name: student.name,
+        belt: student.belt,
+        photoUrl: url,
     });
 };
 
@@ -25,7 +18,7 @@ export const getStudents = async () => {
     const querySnapshot = await getDocs(collection(db, 'alunos'));
     const students: IStudent[] = [];
     querySnapshot.forEach(doc => {
-        students.push(doc.data() as IStudent);
+        students.push({ ...doc.data(), id: doc.id } as IStudent);
     });
     return students;
 };
@@ -33,4 +26,23 @@ export const getStudents = async () => {
 export const deleteStudent = async (id: string, photoUrl: string) => {
     await deleteDoc(doc(db, 'alunos', id));
     await deleteObject(ref(storage, photoUrl));
+};
+
+export const updateStudent = async (student: IStudent) => {
+    const { id, name, belt, photoFile } = student;
+    if (photoFile) {
+        const photoRef = ref(storage, `alunos/${photoFile.name}`);
+        const snapshot = await uploadBytes(photoRef, photoFile);
+        const url = await getDownloadURL(snapshot.ref);
+        await updateDoc(doc(db, 'alunos', id), {
+            name,
+            belt,
+            photoUrl: url,
+        });
+    } else {
+        await updateDoc(doc(db, 'alunos', id), {
+            name,
+            belt,
+        });
+    }
 };
