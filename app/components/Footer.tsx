59ee1f81b9b49b299d59368b5d3648791fc2b0f7
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2024-04-18 18:06:55 -0300

    feat: adiciona informação de fonte das imagens

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -10,11 +10,19 @@ export default function Footer() {
             <ThemeProvider theme={theme}>
                 <Container component='footer' sx={{ textAlign: 'center', py: 2 }}>
                     <Typography variant='body1'>
-                        Desenvolvido por
-                        <Link component={NextLink} href='https://aliine98.github.io' sx={{ px: 1 }}>
+                        Desenvolvido por{' '}
+                        <Link target='_blank' href='https://aliine98.github.io'>
                             Aline Bevilacqua
-                        </Link>
+                        </Link>{' '}
                         &copy; 2024
+                    </Typography>
+                    <Typography variant='body2'>
+                        <em>
+                            Imagens tiradas do site{' '}
+                            <Link target='_blank' href='https://unsplash.com/'>
+                                Unsplash
+                            </Link>
+                        </em>
                     </Typography>
                 </Container>
             </ThemeProvider>
