7f2ea84ffa2ca6264074a9f8ef8365bbca6b4a36
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2024-04-20 17:18:17 -0300

    refactor: usa método map com as infos dos professores num array, eliminando repetição de código

--- app/professores/page.tsx
+++ app/professores/page.tsx
@@ -4,6 +4,27 @@ import React from 'react';
 import Image from 'next/image';
 
 export default function AboutTeachers() {
+    const teachers = [
+        {
+            name: 'Fernanda Silva',
+            image: 'https://firebasestorage.googleapis.com/v0/b/dojo-aguia.appspot.com/o/galeria%2Ffernanda.jpg?alt=media&token=aa7544b0-6803-4e26-9043-8eeb6658f596',
+            description:
+                'Sensei Fernanda é uma instrutora dedicada e atenciosa, com foco no desenvolvimento individual de cada aluno. Ela é especialista em técnicas tradicionais de karatê e em treinamento de kihon.',
+        },
+        {
+            name: 'Bruno Oliveira',
+            image: 'https://firebasestorage.googleapis.com/v0/b/dojo-aguia.appspot.com/o/galeria%2Fbruno.jpg?alt=media&token=faf158e6-1cdf-477d-a223-a5d01c7f2ed0',
+            description:
+                'Sensei Bruno é um instrutor energético e motivador, com grande experiência em competições de karatê. Ele é especialista em treinamento físico e em preparar seus alunos para competições.',
+        },
+        {
+            name: 'Akira Tanaka',
+            image: 'https://firebasestorage.googleapis.com/v0/b/dojo-aguia.appspot.com/o/galeria%2Fakira.jpg?alt=media&token=b7687238-92c5-4ed4-af68-5e4c9516bf7c',
+            description:
+                'Sensei Akira é um instrutor energético e motivador, com grande experiência em competições de karatê. Ele é especialista em treinamento físico e em preparar seus alunos para competições.',
+        },
+    ];
+
     return (
         <>
             <ThemeProvider theme={theme}>
@@ -13,63 +34,19 @@ export default function AboutTeachers() {
                         Professores
                     </Typography>
                     <Box>
-                        <Grid container sx={{ justifyContent: 'center', alignItems: 'center', mb: 4 }}>
-                            <Grid item xs={12} sm={6} sx={{ display: 'flex', justifyContent: 'center' }}>
-                                <Image
-                                    src='https://firebasestorage.googleapis.com/v0/b/dojo-aguia.appspot.com/o/galeria%2Ffernanda.jpg?alt=media&token=aa7544b0-6803-4e26-9043-8eeb6658f596'
-                                    alt='Fernanda'
-                                    width={280}
-                                    height={300}
-                                />
+                        {teachers.map(({ name, image, description }, index) => (
+                            <Grid container key={index} sx={{ justifyContent: 'center', alignItems: 'center', mb: 4 }}>
+                                <Grid item xs={12} sm={6} sx={{ display: 'flex', justifyContent: 'center' }}>
+                                    <Image src={image} alt={'Foto de ' + name} width={280} height={300} />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <Typography variant='h3' component='h2'>
+                                        {name}
+                                    </Typography>
+                                    <Typography variant='body1'>{description}</Typography>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <Typography variant='h3' component='h2'>
-                                    Fernanda Silva
-                                </Typography>
-                                <Typography variant='body1'>
-                                    Sensei Fernanda é uma instrutora dedicada e atenciosa, com foco no desenvolvimento individual de cada aluno. Ela é
-                                    especialista em técnicas tradicionais de karatê e em treinamento de kihon.
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                        <Grid container sx={{ justifyContent: 'center', alignItems: 'center', mb: 4 }}>
-                            <Grid item xs={12} sm={6} sx={{ display: 'flex', justifyContent: 'center' }}>
-                                <Image
-                                    src='https://firebasestorage.googleapis.com/v0/b/dojo-aguia.appspot.com/o/galeria%2Fbruno.jpg?alt=media&token=faf158e6-1cdf-477d-a223-a5d01c7f2ed0'
-                                    alt='Bruno'
-                                    width={280}
-                                    height={300}
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <Typography variant='h3' component='h2'>
-                                    Bruno Oliveira
-                                </Typography>
-                                <Typography variant='body1'>
-                                    Sensei Bruno é um instrutor energético e motivador, com grande experiência em competições de karatê. Ele é
-                                    especialista em treinamento físico e em preparar seus alunos para competições.
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                        <Grid container sx={{ justifyContent: 'center', alignItems: 'center' }}>
-                            <Grid item xs={12} sm={6} sx={{ display: 'flex', justifyContent: 'center' }}>
-                                <Image
-                                    src='https://firebasestorage.googleapis.com/v0/b/dojo-aguia.appspot.com/o/galeria%2Fakira.jpg?alt=media&token=b7687238-92c5-4ed4-af68-5e4c9516bf7c'
-                                    alt='Akira'
-                                    width={280}
-                                    height={300}
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <Typography variant='h3' component='h2'>
-                                    Akira Tanaka
-                                </Typography>
-                                <Typography variant='body1'>
-                                    Sensei Akira é um instrutor energético e motivador, com grande experiência em competições de karatê. Ele é
-                                    especialista em treinamento físico e em preparar seus alunos para competições.
-                                </Typography>
-                            </Grid>
-                        </Grid>
+                        ))}
                     </Box>
                 </Container>
             </ThemeProvider>
